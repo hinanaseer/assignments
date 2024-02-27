8b0dbc0346840477391f8e8cc80dc8cf00905fa8
hina naseer <99113701+hinanaseer@users.noreply.github.com>
2024-02-27 19:25:07 +0500

    first commit

---
+++ firstclass01/main.js
@@ -0,0 +1,9 @@
+"use strict";
+let user_name = "hina"; // type inference
+user_name = "123";
+let user_name1 = "hunain";
+let password = true;
+password = 1234;
+console.log(user_name1 == "hunain");
+console.log("2+3");
+console.log(user_name);
---
+++ firstclass01/main.ts
@@ -0,0 +1,8 @@
+let user_name = "hina" // type inference
+user_name = "123"
+let user_name1: string = "hunain"
+let password: any = true
+password = 1234
+console.log(user_name1 == "hunain")
+console.log("2+3")
+console.log(user_name)
